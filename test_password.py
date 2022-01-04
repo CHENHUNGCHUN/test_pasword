8e1821bf45cdeb8bdb15cdfa2310240b5482b86b
Marco Chen <kojunjunla@outlook.com>
2022-01-04 18:07:38 +0800

    updat2

--- test_password.py
+++ test_password.py
@@ -7,5 +7,7 @@ while count < 3 :
 	else:
 		count += 1
 		try_times = 3 - count
-		print('密碼錯誤 還有{}次機會'.format(try_times))
-	
+		if try_times > 0:
+			print('密碼錯誤 還有{}次機會'.format(try_times))
+		else:
+			print('還是錯 ，帳號封鎖')	
